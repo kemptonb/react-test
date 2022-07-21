27207f6fbbd380684b40df6985cb06a1fb10578a
bradley <bradleybkempton@gmail.com>
2022-07-20 20:34:17 -0700

    made react app that renders lists

--- task-app/src/App.js
+++ task-app/src/App.js
@@ -1,26 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          Hi Lucas!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import Overview from "./components/Overview";
+import uniqid from "uniqid";
+import './style.css';
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      task: {
+        text: '',
+        id: uniqid()
+      },
+      tasks: [],
+    };
+  }
+
+  render() {
+    const { task, tasks } = this.state;
+
+    return (
+      <div className="container">
+        <h2>Render Lists</h2>
+        <form onSubmit={this.onSubmitTask}>
+          <label htmlFor="taskInput">Enter task</label>
+          <input
+            onChange={this.handleChange}
+            value={task.text}
+            type="text"
+            id="taskInput"
+          />
+          <button type="submit">
+            Add Task
+          </button>
+
+        </form>
+
+        <Overview tasks={tasks} />
+
+      </div>
+    );
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      task: {
+        text: e.target.value,
+      }
+    });
+  };
+
+  onSubmitTask = (e) => {
+    e.preventDefault();
+    this.setState({
+      tasks: this.state.tasks.concat(this.state.task),
+      task: { text: '' },
+    });
+  };
 }
 
 export default App;
+
